bbf120d2d8fabbe5b1fbce83606d6976b44613bc
Edery <58280210+tearbird99@users.noreply.github.com>
2025-05-01 14:37:02 +0900

    Ribbon 코드 정리

--- my-app/src/components/ribbonComponents/InsertRibbon.tsx
+++ my-app/src/components/ribbonComponents/InsertRibbon.tsx
@@ -1,4 +1,11 @@
 import './Ribbon.css';
+import newSlideIcon from '../../assets/icons/NewSlide.png';
+import textBoxIcon from '../../assets/icons/TextBox.png';
+import tableIcon from '../../assets/icons/Table.png';
+import pictureIcon from '../../assets/icons/Picture.png';
+import videoIcon from '../../assets/icons/Video.png';
+import shapeIcon from '../../assets/icons/Shape.png';
+import audioIcon from '../../assets/icons/Audio.png';
 
 interface InsertRibbonProps {
   onAddSlide: () => void;
@@ -6,19 +13,25 @@ interface InsertRibbonProps {
 }
 
 function InsertRibbon({ onAddSlide, onAddTextBox }: InsertRibbonProps) {
+  const ribbonItems = [
+    { icon: newSlideIcon, label: '새 슬라이드', onClick: onAddSlide },
+    { icon: textBoxIcon, label: '텍스트 상자', onClick: onAddTextBox },
+    { icon: tableIcon, label: '표' },
+    { icon: pictureIcon, label: '그림' },
+    { icon: videoIcon, label: '비디오' },
+    { icon: audioIcon, label: '오디오' },
+    { icon: shapeIcon, label: '도형' },
+  ];
+
   return (
     <div className="ribbon-container px-3 py-2 border-bottom bg-white">
       <div className="d-flex flex-wrap align-items-center gap-3">
-        <div onClick={onAddSlide}>
-          <img src="../assets/icons/NewSlide.png"/>
-          새 슬라이드
-        </div>
-        <div onClick={onAddTextBox}>텍스트 상자</div>
-        <div>표</div>
-        <div>카메오</div>
-        <div>비디오</div>
-        <div>오디오</div>
-        <div>도형</div>
+        {ribbonItems.map(({ icon, label, onClick }) => (
+          <div className="ribbon-button" key={label} onClick={onClick}>
+            <img src={icon} alt={label} width={18} height={18} />
+            {label}
+          </div>
+        ))}
       </div>
     </div>
   );
